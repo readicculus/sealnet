b85e969293f85540791a2b1ae530dab6fe6495a8
readicculus <yuval@yuvalboss.com>
2019-05-12 22:50:20 -0700

    kinda f'ed up but fast need to fix things

--- net.py
+++ net.py
@@ -1,36 +1,65 @@
 import argparse
+import datetime
 import os
 import time
 
 import torch
 import torchvision.models as models
+from terminaltables import AsciiTable
 from torch import nn
 from torch.autograd import Variable
 
 from models.yolov3.models import Darknet
+# from models.yolov3.test import evaluate
+from models.yolov3.utils.logger import Logger
 from models.yolov3.utils.utils import weights_init_normal
 from train_loader import get_train_loader
+# from terminaltables import AsciiTable
 
+logger = Logger("logs")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model = Darknet("yolov3.cfg", img_size=640).to(device)
-# model.apply(weights_init_normal)
-# model.load_darknet_weights("/data/pretrained_weights/darknet53.conv.74")
+model = Darknet("/home/yuval/Documents/XNOR/sealnet/models/yolov3/yolov3.cfg", img_size=640).to(device)
+model.apply(weights_init_normal)
+model.load_darknet_weights("/data/pretrained_weights/darknet53.conv.74")
 
-model = models.alexnet(pretrained=True).to(device)
-model.classifier[6] = nn.Linear(4096, 4)
-loss = models.AlexNet(num_classes=3).to(device)
 parser = argparse.ArgumentParser(description='Process images for new dataset')
 parser.add_argument('-c', '--config', dest='config_path', required=True)
-parser.add_argument('-gpu', type=int, default=[1], nargs='*', help='used gpu')
+parser.add_argument('-gpu', type=int, default=[1], nargs='+', help='used gpu')
 args = parser.parse_args()
 
-os.environ["CUDA_VISIBLE_DEVICES"] = ''.join(str(x) for x in args.gpu)
+# os.environ["CUDA_VISIBLE_DEVICES"] = ''.join(str(x) for x in args.gpu)
+os.environ["CUDA_VISIBLE_DEVICES"] = '12'
 
-dataloader = get_train_loader(args.config_path, 16, 1)
-for epoch in range(100):
+print("GPUS" + os.environ["CUDA_VISIBLE_DEVICES"])
+dataloader = get_train_loader(args.config_path, 2, 8)
+
+optimizer = torch.optim.Adam(model.parameters())
+
+metrics = [
+    "grid_size",
+    "loss",
+    "x",
+    "y",
+    "w",
+    "h",
+    "conf",
+    "cls",
+    "cls_acc",
+    "recall50",
+    "recall75",
+    "precision",
+    "conf_obj",
+    "conf_noobj",
+]
+
+EPOCHS = 100
+IM_SIZE = 640
+EVAL_INTERVAL = 1
+CHKPT_INTERVAL = 10
+class_names = ["Ringed", "Bearded", "UNK"]
+
+for epoch in range(EPOCHS):
     model.train()
-    loss.train()
-
     start_time = time.time()
     print(epoch)
     for batch_i, (_, imgs, targets) in enumerate(dataloader):
@@ -40,6 +69,71 @@ for epoch in range(100):
         imgs = Variable(imgs.to(device))
         targets = Variable(targets.to(device), requires_grad=False)
 
-        outputs = model(imgs)
-        loss_output = loss(outputs, targets)
-        x=1
+        loss, outputs = model(imgs, targets)
+        if batches_done % 2:
+            # Accumulates gradient before each step
+            optimizer.step()
+            optimizer.zero_grad()
+
+        log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, EPOCHS, batch_i, len(dataloader))
+
+        metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
+
+        # Log metrics at each YOLO layer
+        for i, metric in enumerate(metrics):
+            formats = {m: "%.6f" for m in metrics}
+            formats["grid_size"] = "%2d"
+            formats["cls_acc"] = "%.2f%%"
+            row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in model.yolo_layers]
+            metric_table += [[metric, *row_metrics]]
+
+            # Tensorboard logging
+            tensorboard_log = []
+            for j, yolo in enumerate(model.yolo_layers):
+                for name, metric in yolo.metrics.items():
+                    if name != "grid_size":
+                        tensorboard_log += [(f"{name}_{j+1}", metric)]
+            tensorboard_log += [("loss", loss.item())]
+            logger.list_of_scalars_summary(tensorboard_log, batches_done)
+
+        log_str += AsciiTable(metric_table).table
+        log_str += f"\nTotal loss {loss.item()}"
+
+        # Determine approximate time left for epoch
+        epoch_batches_left = len(dataloader) - (batch_i + 1)
+        time_left = datetime.timedelta(seconds=epoch_batches_left * (time.time() - start_time) / (batch_i + 1))
+        log_str += f"\n---- ETA {time_left}"
+
+        print(log_str)
+
+        model.seen += imgs.size(0)
+
+        if epoch % EVAL_INTERVAL == 0 and False:
+            print("\n---- Evaluating Model ----")
+            # Evaluate the model on the validation set
+            precision, recall, AP, f1, ap_class = evaluate(
+                model,
+                path=valid_path,
+                iou_thres=0.5,
+                conf_thres=0.5,
+                nms_thres=0.5,
+                img_size=IM_SIZE,
+                batch_size=8,
+            )
+            evaluation_metrics = [
+                ("val_precision", precision.mean()),
+                ("val_recall", recall.mean()),
+                ("val_mAP", AP.mean()),
+                ("val_f1", f1.mean()),
+            ]
+            logger.list_of_scalars_summary(evaluation_metrics, epoch)
+
+            # Print class APs and mAP
+            ap_table = [["Index", "Class name", "AP"]]
+            for i, c in enumerate(ap_class):
+                ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+            print(AsciiTable(ap_table).table)
+            print(f"---- mAP {AP.mean()}")
+
+        if epoch != 0 and epoch % CHKPT_INTERVAL == 0:
+            torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
--- train_loader.py
+++ train_loader.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import sys
 
 import torch
 from PIL import Image
@@ -7,40 +8,74 @@ from torch.utils.data import Dataset
 from torch.utils import data as data_utils
 import numpy as np
 from torchvision.transforms import transforms
+import torch.nn.functional as F
 
-from utils import get_train_test_meta_data, get_train_test_base
+import dataset.utils as utils
+import dataset.transforms as transformsx
+from models.yolov3.utils.augmentations import horisontal_flip
 
+def pad_to_square(img, pad_value):
+    c, h, w = img.shape
+    dim_diff = np.abs(h - w)
+    # (upper / left) padding and (lower / right) padding
+    pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
+    # Determine padding
+    pad = (0, 0, pad1, pad2) if h <= w else (pad1, pad2, 0, 0)
+    # Add padding
+    img = F.pad(img, pad, "constant", value=pad_value)
+
+    return img, pad
+
+def resize(image, size):
+    image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
+    return image
 
 class TrainLoader(Dataset):
     def __init__(self, config, img_size=640, augment=True, multiscale=True, normalized_labels=True):
         try:
+            sys.modules['utils'] = utils
+            sys.modules['transforms'] = transformsx
             pickle_file = open(config, 'rb')
             config = pickle.load(pickle_file)
         except:
             raise Exception("Could not load file " + config)
 
-        self.train_meta, _ = get_train_test_meta_data(config)
+        self.train_meta, _ = utils.get_train_test_meta_data(config)
 
-        self.train_path, _ = get_train_test_base(config)
+        self.train_path, _ = utils.get_train_test_base(config)
         self.images = list(self.train_meta.keys())
         self.batch_count = 0
         self.img_size = img_size
+        self.normalized_labels = normalized_labels
+        self.augment = augment
+        self.size = len(self.train_meta)
 
     def __len__(self):
-        return len(self.train_meta)
+        return self.size
 
     def __getitem__(self, idx):
-        img = self.images[idx]
-        image_path = os.path.join(self.train_path, img)
+        img_name = self.images[idx]
+        image_path = os.path.join(self.train_path, img_name)
+        img = transforms.ToTensor()(Image.open(image_path).convert('RGB')).float()
+        _, h, w = img.shape
+        h_factor, w_factor = (h, w) if self.normalized_labels else (1, 1)
+        # Pad to square resolution
+        img, pad = pad_to_square(img, 0)
+        _, padded_h, padded_w = img.shape
 
-        hotspots = self.train_meta[img].hotspots
+        hotspots = self.train_meta[img_name].hotspots
         hs_mat = np.zeros((len(hotspots), 6))
         for i, hs in enumerate(hotspots):
-            hs_mat[i] = [0, hs.label[0], hs.x1, hs.y1, hs.x2, hs.y2]
+            cx = ((hs.x1 + hs.x2)/2)/padded_w
+            cy = ((hs.y1 + hs.y2)/2)/padded_h
+            w = (hs.x2 - hs.x1)/padded_w
+            h = (hs.y2 - hs.y1)/padded_h
+            hs_mat[i] = [0, hs.label[0], cx, cy, w,h]
 
-        targets = torch.from_numpy(hs_mat)
-        img = transforms.ToTensor()(Image.open(image_path).convert('RGB'))
-
+        targets = torch.from_numpy(hs_mat).float()
+        if self.augment:
+            if np.random.random() < 0.5:
+                img, targets = horisontal_flip(img, targets)
         return image_path, img, targets
 
 
@@ -51,12 +86,12 @@ class TrainLoader(Dataset):
         for i, boxes in enumerate(targets):
             boxes[:, 0] = i
         targets = torch.cat(targets, 0)
-        imgs = torch.cat(imgs, 0)
+        # imgs = torch.cat(imgs, 0)
         # Selects new image size every tenth batch
         # if self.multiscale and self.batch_count % 10 == 0:
         #     self.img_size = torch.random.choice(range(self.min_size, self.max_size + 1, 32))
         # Resize images to input shape
-        # imgs = torch.stack([np.resize(img, self.img_size) for img in imgs])
+        imgs = torch.stack([resize(img, self.img_size) for img in imgs])
         self.batch_count += 1
         return paths, imgs, targets
 
